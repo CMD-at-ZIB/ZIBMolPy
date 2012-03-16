72ebeebfdc5d1f3df3531874856381329b0f2155
Alexander Bujotzek <bujotzek@zib.de>
2012-03-16 18:25:20 +0100

    added script for making links

---
+++ make_links.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# for all py-files in tools/ creates convenient links (and drops extension '.py') 
+
+# removing extra slash from $1
+LINKDEST=${1%\/}
+
+if [ ! -d $LINKDEST ] ; then
+	echo $LINKDEST" is not a directory."
+	exit 1
+fi
+
+cd tools/
+
+for file in *.py;
+do
+	# check if file is a file
+	if [ -f $file ] ; then
+		# get name without extension
+		name=${file%\.*}
+		echo "Making link for "${name}" ..."
+		ln -s $PWD/$file $LINKDEST/$name
+	fi
+done
+
+#EOF
