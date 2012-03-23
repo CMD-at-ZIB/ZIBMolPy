3896ee8e0b08c0a5ad0ab0577b1a32b6b2c66cb2
Ole Schütt <ole@schuett.name>
2012-03-23 19:02:56 +0100

    added skeleton for zgf_gauge_times

---
+++ tools/zgf_gauge_times.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from ZIBMolPy.pool import Pool
+
+
+def is_applicable():
+	return(True)
+	
+	
+#===============================================================================
+def main():
+	pool = Pool()
+	for n in pool.where("isa_partition"):
+		for cn in n.children.where("is_sampled"):
+			print cn.trajectory
+		
+#===============================================================================
+if(__name__ == "__main__"):
+	main()
+
+#EOF
