e856633a6cd0400caaf79c986e6f5ac0599d474e
Ole Schütt <ole@schuett.name>
2012-03-23 16:16:47 +0100

    unified install-scripts into scripts/installer.py

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@
 all:
 	@echo "make install - Install on local system"
 	@echo "make install-home - Install for this user"
-#	@echo "make debuild - Generate a deb package"
 	@echo "make clean - Get rid of scratch and byte files"
 	@echo "make docu - Generate API documentation"
 	@echo "make upload-docu - Upload API-docu to webserver"
@@ -12,56 +11,27 @@ all:
 
 docu:
 #	use python-modules from source - not the ones installed on the system
-	@export PYTHONPATH=./ZIBMolPy_package/:$(PYTHONPATH); epydoc --conf=epydoc.conf
+	@export PYTHONPATH=./ZIBMolPy_package/:$(PYTHONPATH); epydoc --conf=./scripts/epydoc.conf
 
 upload-docu:
-	./upload_docu.sh	
+	./scripts/upload_docu.sh	
 
 install-home:
-	cd ZIBMolPy_package; ./setup.py install --prefix=$(HOME)
-	mkdir -vp $(HOME)/bin/
-	mkdir -vp $(HOME)/share/zibmolpy/
-	cd ZIBMolPy_package; ./make_links.sh $(HOME)/bin/
-	rsync -av --exclude='*/.*' --exclude="*.pyc" tests $(HOME)/share/zibmolpy/
+	./scripts/installer.py --prefix=$(HOME) install
 	
 install:
-	cd ZIBMolPy_package; ./setup.py install --root $(DESTDIR)/ $(COMPILE)
-	mkdir -vp $(DESTDIR)/usr/bin/
-	mkdir -vp $(DESTDIR)/usr/share/zibmolpy/
-	cd ZIBMolPy_package; ./make_links.sh $(DESTDIR)/usr/bin/
-#TODO: remove test-pools and temp-files
-	rsync -av --exclude='*/.*' tests $(DESTDIR)/usr/share/zibmolpy/
-	
-#debuild:
-#	debuild  -eCOMPILE="--no-compile"
+	./scripts/installer.py install
 
 clean:
 	rm -rvf ZIBMolPy_package/build
 	rm -rvf ./apidocs
 
 todo:
-	grep --color -r --exclude-dir="build" --exclude-dir=".svn" --include="*.py" "TODO" *
+	grep --color -r --exclude-dir="build" --exclude-dir=".*" --include="*.py" "TODO" *
 
 pylint:
-	cd tools; pylint --rcfile=../pylintrc `find ../ZIBMolPy_package/ZIBMolPy/ -name \*.py` ./zgf_*.py 
+	cd tools; pylint --rcfile=../scripts/pylintrc `find ../ZIBMolPy_package/ZIBMolPy/ -name \*.py` ./zgf_*.py 
 
-# tag:
-# - svn copy to create a tag
-# - svn commit mit passendem comment
-# - debian changelog akutuallisieren
-
-
-# upload-deb:
-#	# - debian changelog akutuallisieren
-#	reprepro -Vb /global/www/Abt-Numerik/cmd-debian/ubuntu  includedeb lucid ../zibmolpy*.deb
-
-# release:
-# tag
-# install-home
-# docu
-# upload-docu
-# debuild
-# upload-deb
-
-.PHONY: all docu upload-docu install install-home debuild clean todo pylint 
+	
+.PHONY: all docu upload-docu install install-home clean todo pylint 
 #EOF
---
+++ scripts/installer.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from subprocess import Popen
+from optparse import OptionParser
+from os import path
+import shutil
+from glob import glob
+
+
+LOG_FILENAME = "installed_files.log"
+
+#===============================================================================
+def main():
+	usage = 'Usage: %prog [options] <action>\n<action> can be "install" or "uninstall"'
+	parser = OptionParser(usage=usage)
+	parser.add_option("-p", "--prefix", help="base-directory for the installation", default="/")
+	(options, args) = parser.parse_args()
+	if(len(args) != 1):
+		parser.error("wrong number of arguments.")
+	
+	if(args[0] == "install"):
+		do_install(options.prefix)
+	
+	elif(args[0] == "uninstall"):
+		do_uninstall()
+		
+	else:
+		parser.error("unkown action: "+args[0])
+	
+
+
+	
+#===============================================================================
+def do_install(prefix="/"):
+	assert(prefix[-1] == "/")
+		
+	log = open(LOG_FILENAME, "a")
+
+	def mkdir(dirname):
+		log.write(dirname+"\n")
+		if(not path.exists(dirname)):
+			print("Creating dir: "+dirname)
+			os.makedirs(dirname)
+	
+	def copyfile(fn_src, fn_dest):
+		log.write(fn_dest+"\n")
+		print("Copying %s -> %s"%(fn_src, fn_dest))
+		shutil.copy(fn_src, fn_dest)
+		
+	# create destination dirs
+	dir_bin = prefix + "bin/"
+	dir_share = prefix + "usr/share/zibmolpy/"
+	mkdir(dir_bin)
+	mkdir(dir_share)
+		
+	# copy test-cases
+	for root, dirs, files in os.walk('tests'):
+		mkdir(dir_share+root)
+		for fn in files:
+			if fn.endswith('.pyc'): continue
+			if fn.startswith('.'): continue
+			copyfile(root+"/"+fn, dir_share+root+"/"+fn)
+      
+	
+	# install the library modules
+	cmd = ["./setup.py","install"]
+	if(prefix != "/"):
+		cmd += ["--prefix="+prefix]
+	p = Popen(cmd, cwd="ZIBMolPy_package")
+	p.wait()
+	assert(p.returncode == 0)
+	
+	# copy tools and create symlinks
+	for fn in glob("./tools/*.py"):
+		fn_dest = dir_bin+path.basename(fn)
+		copyfile(fn, fn_dest)
+		
+		link_src = path.basename(fn)
+		link_name = fn_dest[:-3]
+		log.write(link_name+"\n")
+		if(not path.exists(link_name)):
+			print("Createing Link %s -> %s"%(link_name, link_src))
+			os.symlink(link_src, link_name)
+			
+	
+	log.close()
+	print("Install done.")
+	
+#===============================================================================
+def do_uninstall():
+	try:
+		#removing library
+		import ZIBMolPy
+		lib_dir = path.dirname(ZIBMolPy.__file__)
+		del(ZIBMolPy)
+		print("Removing directory-tree: "+lib_dir)
+		shutil.rmtree(lib_dir)
+		
+		#removing egg-files
+		for fn in glob(lib_dir+"*.egg-info"):
+			print("Removeing file: "+fn)
+			os.remove(fn)	
+	except:
+		print("Could not uninstall library.")
+		pass
+	
+	
+	#removing tools and test-cases
+	if(not path.exists(LOG_FILENAME)):
+		print("Required file %s not found."%LOG_FILENAME)
+		sys.exit(1)
+	
+	log = open(LOG_FILENAME, "r")
+	for fn in reversed(log.readlines()):
+		fn = fn.strip()
+		if(not path.exists(fn)):
+			pass
+		elif(not path.isdir(fn)):
+			print("Removeing file: "+fn)
+			os.remove(fn)	
+		elif(len(os.listdir(fn)) == 0):
+			print("Removeing directory: "+fn)
+			os.rmdir(fn)
+		else:
+			print("Leaving not empty directory: "+fn)
+				
+			
+	log.close()
+	os.remove(LOG_FILENAME)
+	print("Uninstall done.")
+
+#===============================================================================	
+	
+	
+if(__name__ == "__main__"):
+	main()
+	
+#EOF
