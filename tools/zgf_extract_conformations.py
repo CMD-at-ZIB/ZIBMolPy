5625e43e5a0a6311128e15908a7d513d0b2f6908
Vedat Durmaz <vedat.durmaz@gmx.net>
2014-05-13 15:37:33 +0200

    several changes concerning most py-scripts due to new system of the next extension stage of the high perfomance computer HLRN3

--- tools/zgf_extract_conformations.py
+++ tools/zgf_extract_conformations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from ZIBMolPy.utils import register_file_dependency
--- tools/zgf_gauge_times.py
+++ tools/zgf_gauge_times.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from ZIBMolPy.pool import Pool
